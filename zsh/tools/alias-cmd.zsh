b7f307a16dadc6347a898a05cab4b32596dae6d9
luoluokong10 <luoluokong@icloud.com>
2025-12-30 23:00:23 +0800

    feat(zsh-alias): add OS, distro detection

--- zsh/tools/alias-cmd.zsh
+++ zsh/tools/alias-cmd.zsh
@@ -4,6 +4,26 @@ ALIAS_MAP=(
     fs fastfetch
 )
 
+is_arch() {
+    id=$(grep -oP '^ID=\K\S+' /etc/os-release | head -1)
+    [[ $id == "arch" ]]
+}
+
+case "$(uname -s)" in
+    Linux)
+        if is_arch; then
+            ALIAS_MAP+=(
+                pls "sudo pacman -S"
+            )
+        fi
+        ;;
+    Darwin)
+        ALIAS_MAP+=(
+            pls "brew install"
+        )
+        ;;
+esac
+
 for name cmd in ${(kv)ALIAS_MAP}; do
     alias "$name=$cmd"
 done
